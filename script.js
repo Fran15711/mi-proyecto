6be96c1afdd995be506094381d825c1368acf89f
Fran15711 <francisconoriegaret15@gmail.com>
2024-09-24 16:50:02 -0600

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,19 @@ const slider = document.querySelector('.slider');
 
 function activate(e) {
   const items = document.querySelectorAll('.item');
-  e.target.matches('.next') && slider.append(items[0])
-  e.target.matches('.prev') && slider.prepend(items[items.length-1]);
+  
+  // Verifica si el botón clickeado es el siguiente
+  if (e.target.matches('.next')) {
+    // Mueve el primer elemento al final del slider
+    slider.append(items[0]);
+  }
+
+  // Verifica si el botón clickeado es el anterior
+  if (e.target.matches('.prev')) {
+    // Mueve el último elemento al principio del slider
+    slider.prepend(items[items.length - 1]);
+  }
 }
 
-document.addEventListener('click',activate,false);
+// Escucha el evento de clic en el documento
+document.addEventListener('click', activate, false);
